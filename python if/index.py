3154b9b0f7d857145aa0ae70ab688e5abad6463c
naveen-kumar-078 <naveenkumar071224@gmail.com>
2025-10-18 14:55:14 +0530

    the py

---
+++ python if/index.py
@@ -0,0 +1,76 @@
+# age = 17
+
+# if age>=18:
+#     print("you are adult.")
+# else:
+#     print("you are an minor")
+
+
+# mark = 60
+
+# if mark >=90:
+#     print("Grade: A")
+# elif mark >=80:
+#     print("Grade: B") 
+# elif mark >=70:
+#     print("Grade: C")
+# else:
+#     print("Grade: F")
+
+
+# fruits = ["Apple","orange","banana"]
+
+# for fruits in fruits:
+#     print(fruits)
+
+
+
+# age = 20
+# if age>18:
+#     print("you are elibible to vote")
+# else:
+#     print("you are not eligible to vote")
+
+# mark = 90
+# if mark > 90:
+#     print("GRADE A")
+# elif mark > 80:
+#     print("GRADE B")
+# elif mark> 70:
+#     print("GRADE 70")
+# else:
+#     print("GRADE: F")
+
+
+
+# x = 21
+
+# if x > 0:
+#     print("x is an positive number")
+#     if x < 20:
+#         print("x is lesser than 20")
+
+#     else:
+#         print("x is greater than 20")
+# else:
+#     print("x is an negative number")
+
+
+# Fruits = ["Apple", "Orange", "Mango"]
+
+# for fruit in Fruits:
+#     print("I Like fruits",Fruits)
+
+
+
+# for letter in "python":
+#     print(letter)
+
+
+# for i in range(5):
+#     print(i)
+
+
+
+for i in range(2,6):
+    print(i)
