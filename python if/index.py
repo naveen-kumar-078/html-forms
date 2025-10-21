c6e3424968f627d044197a67c2ea955fbe229aa2
naveen-kumar-078 <naveenkumar071224@gmail.com>
2025-10-21 21:50:31 +0530

    python

--- python if/index.py
+++ python if/index.py
@@ -1,70 +1,110 @@
-# age = 17
+# age=20
 
-# if age>=18:
-#     print("you are adult.")
-# else:
-#     print("you are an minor")
-
-
-# mark = 60
-
-# if mark >=90:
-#     print("Grade: A")
-# elif mark >=80:
-#     print("Grade: B") 
-# elif mark >=70:
-#     print("Grade: C")
-# else:
-#     print("Grade: F")
-
-
-# fruits = ["Apple","orange","banana"]
-
-# for fruits in fruits:
-#     print(fruits)
-
-
-
-# age = 20
 # if age>18:
-#     print("you are elibible to vote")
+#     print("you are eligible")
 # else:
-#     print("you are not eligible to vote")
+#     print("you are not eligible")
 
-# mark = 90
-# if mark > 90:
-#     print("GRADE A")
+
+
+# mark = 66
+
+# if mark>=100:
+#     print("grade a")
 # elif mark > 80:
 #     print("GRADE B")
-# elif mark> 70:
-#     print("GRADE 70")
+# elif mark > 70:
+#     print("Grade C")
 # else:
-#     print("GRADE: F")
+#     print("GRADE F")
 
 
+# num = 19
 
-# x = 21
-
-# if x > 0:
-#     print("x is an positive number")
-#     if x < 20:
-#         print("x is lesser than 20")
-
+# if num > 20:
+#     print("its an positive")
+#     if num %2 == 0:
+#         print("its an even number")
 #     else:
-#         print("x is greater than 20")
+#         print("its an odd number")
 # else:
-#     print("x is an negative number")
+#  print("its an negative number")
+
+# Creating a list (array)
 
 
-# Fruits = ["Apple", "Orange", "Mango"]
+# from array import array
 
-# for fruit in Fruits:
-#     print("I Like fruits",Fruits)
+# numbers = array("i",[20,30,40,50])
+# print(numbers)
+# print(numbers[0])
+# numbers.append(70)
+# numbers.remove(40)
+# print("updated list:",numbers)
+
+
+# import numpy as nk
+
+# numbers = nk.array([10,20,30,40,50])
+# print("Numpy values",numbers)
+# print("first element:",numbers[0])
+# print("updated element:",numbers*2)
 
 
 
-# for letter in "python":
-#     print(letter)
+# the_list = [10,20,30,40]
+
+# print("List of number:",the_list)
+
+# the_list.append(60)
+# the_list.remove(30)
+
+# print("the updated list",the_list)
+
+
+# fruits = ["apple","orange","mango","banana"]
+# print(fruits[0])
+# fruits.append('watermelon')
+# print(fruits)
+
+
+
+
+# array = [1,2,3,3,4]
+# s = set(array)
+# print(s)
+
+
+# marks = {
+#     "kumar":[98,97,96],
+#     "ravi":[88,87,86],
+#     "siva":[78,77,76]
+# }
+
+# marks["jj"]=[90,60,80]
+# print(marks)
+
+# print(marks["jj"][0])
+
+
+
+
+
+# # import array
+
+# # marks = {
+# #     "jessi": array.array("i",[90,95,92,98]),
+# #     "rosy": array.array("i",[10,11,12])
+# # }
+
+# # print(marks["rosy"][0])
+# # print(marks)
+
+
+# fruits = ["apple","orange","mango"]
+
+# for fruit in fruits:
+#     print(fruit)
 
 
 # for i in range(5):
@@ -72,5 +112,11 @@
 
 
 
-for i in range(2,6):
-    print(i)
+#     for letter in "loop":
+        # print(letter)
+
+
+
+for i in range(3):
+    for j in range(2):
+        print(i,j)
